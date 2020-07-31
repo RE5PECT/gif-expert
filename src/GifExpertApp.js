fc0ce56d28968348bb2f4c8882e220bb4f00afe6
Ricardo Vera <ricardo@Ricardos-MacBook-Pro.local>
2020-07-30 22:07:54 -0400

    Avance aplicacion

---
+++ src/GifExpertApp.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch']);
+
+  /* const handleAdd = () => {
+    setCategories([...categories, 'Simpsons']);
+  }; */
+
+  return (
+    <>
+      <h2>GIF Expert App</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      <ol>
+        {categories.map((category) => {
+          return <GifGrid key={category} category={category} />;
+        })}
+      </ol>
+    </>
+  );
+};
---
+++ src/components/GifGrid.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    getGifts();
+  }, []);
+
+  const getGifts = async () => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=10&api_key=fgHkovrv57h8KnJU5r3L1MeYS2Pkf1jt`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
+    setImages(gifs);
+  };
+
+  return (
+    <>
+      <h3>{category}</h3>
+      <div className='card-grid'>
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
+  );
+};
