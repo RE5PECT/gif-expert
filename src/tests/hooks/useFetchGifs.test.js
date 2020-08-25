eac1643901058816db57af4fdfd6e7aa26ad2f40
RE5PECT <ricardo.vera4@gmail.com>
2020-08-24 23:39:35 -0400

    corrige bug pruebas

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -1,11 +1,11 @@
 import { useFetchGifs } from '../../hooks/useFetchGifs';
 import { renderHook } from '@testing-library/react-hooks';
 describe('Test on custom hoom useFetchGifs', () => {
-  test('should return initial state', () => {
-    const { result } = renderHook(() => useFetchGifs('One Punch'));
+  test('should return initial state', async() => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('One Punch'));
     const { data, loading } = result.current;
 
-    console.log(data, loading);
+    await waitForNextUpdate();
 
     expect(data).toEqual([]);
     expect(loading).toBe(true);
@@ -16,8 +16,6 @@ describe('Test on custom hoom useFetchGifs', () => {
     );
     await waitForNextUpdate();
     const { data, loading } = result.current;
-
-    console.log(data, loading);
 
     expect(data.length).toBe(10);
     expect(loading).toBe(false);
